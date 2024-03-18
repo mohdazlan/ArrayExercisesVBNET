c654e8c34ceae813440db051d77a2e76bd33d248
mohdazlan <azlan@pmu.edu.my>
2024-03-18 08:29:16 +0800

    added BMI-List gridview

--- frmBMIDynamic.vb
+++ frmBMIDynamic.vb
@@ -3,10 +3,23 @@
     Private Sub frmBMIDynamic_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         DataGridViewStudents.Columns.Add("Nama Pelajar", "Name")
         DataGridViewStudents.Columns.Add("Bacaan BMI", "BMI")
+
+        UpdateDataGridView()
+    End Sub
+    Sub UpdateDataGridView()
+        DataGridViewStudents.Rows.Clear()
+        For Each student In students
+            DataGridViewStudents.Rows.Add(student)
+        Next
+
     End Sub
 
     Private Sub ButtonAddStudent_Click(sender As Object, e As EventArgs) Handles ButtonAddStudent.Click
         Dim name As String = TextBoxName.Text.Trim()
         Dim bmi As String = TextBoxBMI.Text.Trim()
+
+        students.Add({name, bmi})
+
+        UpdateDataGridView()
     End Sub
 End Class
