35a7a83e2dd37556004bee91bcc862c7c4cd2371
mohdazlan <azlan@pmu.edu.my>
2024-03-18 08:09:51 +0800

    second commit

---
+++ frmBMIDynamic.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBMIDynamic
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridViewStudents = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ButtonAddStudent = New System.Windows.Forms.Button()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.TextBoxBMI = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.DataGridViewStudents, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridViewStudents
+        '
+        Me.DataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridViewStudents.Location = New System.Drawing.Point(12, 59)
+        Me.DataGridViewStudents.Name = "DataGridViewStudents"
+        Me.DataGridViewStudents.Size = New System.Drawing.Size(285, 150)
+        Me.DataGridViewStudents.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 234)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Average BMI Result"
+        '
+        'ButtonAddStudent
+        '
+        Me.ButtonAddStudent.Location = New System.Drawing.Point(12, 250)
+        Me.ButtonAddStudent.Name = "ButtonAddStudent"
+        Me.ButtonAddStudent.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonAddStudent.TabIndex = 2
+        Me.ButtonAddStudent.Text = "Add Data"
+        Me.ButtonAddStudent.UseVisualStyleBackColor = True
+        '
+        'TextBoxName
+        '
+        Me.TextBoxName.Location = New System.Drawing.Point(197, 7)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxName.TabIndex = 3
+        '
+        'TextBoxBMI
+        '
+        Me.TextBoxBMI.Location = New System.Drawing.Point(197, 33)
+        Me.TextBoxBMI.Name = "TextBoxBMI"
+        Me.TextBoxBMI.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxBMI.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 14)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 36)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "BMI Reading"
+        '
+        'frmBMIDynamic
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(312, 279)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBoxBMI)
+        Me.Controls.Add(Me.TextBoxName)
+        Me.Controls.Add(Me.ButtonAddStudent)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridViewStudents)
+        Me.Name = "frmBMIDynamic"
+        Me.Text = "frmBMIDynamic"
+        CType(Me.DataGridViewStudents, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridViewStudents As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ButtonAddStudent As Button
+    Friend WithEvents TextBoxName As TextBox
+    Friend WithEvents TextBoxBMI As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ frmBMIDynamic.vb
@@ -0,0 +1,12 @@
+﻿Public Class frmBMIDynamic
+    Dim students As New List(Of String())
+    Private Sub frmBMIDynamic_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DataGridViewStudents.Columns.Add("Nama Pelajar", "Name")
+        DataGridViewStudents.Columns.Add("Bacaan BMI", "BMI")
+    End Sub
+
+    Private Sub ButtonAddStudent_Click(sender As Object, e As EventArgs) Handles ButtonAddStudent.Click
+        Dim name As String = TextBoxName.Text.Trim()
+        Dim bmi As String = TextBoxBMI.Text.Trim()
+    End Sub
+End Class
